319318e29ebcab4928945039162e47cb5570d1b7
Ishwor Shah <166372025+IshworShah@users.noreply.github.com>
2024-08-30 07:36:59 +0000

    Conversion of Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,72 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let stringInNumber = String(someNumber);
-console.log(stringInNumber);
-console.log(typeof(stringInNumber));
+// console.log(stringInNumber);
+// console.log(typeof(stringInNumber));
+
+
+
+// *************************************Operations****************************************************
+
+
+
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+
+let value = 100;
+let negValue = -value;
+// console.log(negValue);
+
+
+
+let str1 = "ishwor"
+let str2 =  " shah"
+// console.log(str1 + str2);
+
+// console.log("2" + 1);
+// console.log(2 + "1");
+// console.log("2" + 1 + 1);
+// console.log(2 + 1 + "1");
+
+// console.log(+true);
+// console.log(+false);
+// console.log(-"");
+
+
+
+let gameCounter = 100;
+--gameCounter;
+gameCounter++;
+
+// console.log(gameCounter);
+
+// prefix and postfix increment/decrement
+// increment
+let b, a = 4;
+b = ++a;     // expected result(prefix) => a => 5, b => 5   
+// console.log(a,b);
+
+let y, x=7;
+y = x++;    // expected result(postfix) => x => 8, y => 7
+// console.log(x,y);
+
+// decrement
+let d, c = 4;
+d = --c;     // expected result(prefix) => c => 3, d => 3   
+// console.log(c,d);
+
+let g, f=7;
+g = f--;    // expected result(postfix) => f => 6, g => 7
+// console.log(f,g);
+
+
+
+
 
