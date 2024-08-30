3904fa925899c9690444d200a8c0ead0eaba2616
Ishwor Shah <166372025+IshworShah@users.noreply.github.com>
2024-08-30 06:36:49 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "ishwor" 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" =>33
+// "33abc" => NaN
+//  true => 1
+//  false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true  
+// 0 => false
+// "ishwor" = true
+// " " => false
+
+let someNumber = 33
+
+let stringInNumber = String(someNumber);
+console.log(stringInNumber);
+console.log(typeof(stringInNumber));
+
