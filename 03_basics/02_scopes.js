cb23cc36ccbb881fdb08d50bb21da41dfa7b4844
Ishwor Shah <166372025+IshworShah@users.noreply.github.com>
2024-09-16 08:04:59 +0000

    Scopes JS

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,39 @@
+// // {} => its called object declaration when its comes with object.... 
+// // {} => its called scope when its use with function and if/else or anything in programming language ....eg: scope of function or program
+
+
+// //global scope          // Variables declared outside the {} block called global scope.
+// //Global scope variables are available on block scope, but the value written inside block scope should not go outside, i.e., global scope.
+     
+// let a = 500
+// const b = 600
+// var c = 400     
+
+// //block scope       // Variables declared inside the {} block called block scope.
+// //Variables declared inside a { } block cannot be accessed from outside the block:
+
+// if(true){
+// let a = 10
+// const b = 20   
+// // var c = 30          // Variables declared with the var keyword can NOT have block scope. 
+
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+// }
+
+// // In simpler terms, the code written inside the block scope {} should operate inside the block scope only...
+// // But code written in the global scope i.e. outside of block scope { } can also be accessed and operated on from within the block scope.
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+// //Notes : The global scope you check in the browser console and the example you run in Node.js in your code environment are different.
+
+
+
+
+
+
+
